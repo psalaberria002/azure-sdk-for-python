5fdd21897b6ca908cb89e1f340eca2ee2cebe964
Scott Beddall (from Dev Box) <scbedd@microsoft.com>
2023-10-27 12:39:05 -0700

    remove print in Save-Package-Properties

--- eng/common/scripts/Save-Package-Properties.ps1
+++ eng/common/scripts/Save-Package-Properties.ps1
@@ -94,9 +94,6 @@ function GetRelativePath($path) {
 $exportedPaths = @{}
 $allPackageProperties = Get-AllPkgProperties $serviceDirectory
 
-Write-Host "This is what we got for allPackageProperties, which will be empty improperly"
-Write-Host $allPackageProperties
-
 if ($allPackageProperties)
 {
     if (-not (Test-Path -Path $outDirectory))
